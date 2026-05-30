a8a8b6beb0b0c8a1f5ee06b42551162008b0ce7a
Alejandro Trejo <istrejo2106@gmail.com>
2026-05-30 20:44:57 +0100

    fix: separate Tailwind CSS from SCSS to avoid Dart Sass deprecation warning
    PostCSS processes tailwind.css, Sass handles styles.scss independently

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
 
+import '../src/tailwind.css';
 import '../src/styles.scss';
 
 setCompodocJson(docJson);
